85e9ba0043cf4b32efcd65bd76c68aa9a723f301
nicolasBernaux <bernaux.n@gmail.com>
2022-10-03 18:18:00 +0200

    First commit

---
+++ content.js
@@ -0,0 +1,13 @@
+module.exports = {
+  metaTitle: 'Agence communication La Rochelle',
+  metaDescription: 'Agence communication La Rochelle',
+  title: 'Agence communication La Rochelle',
+  keywords: 'Référencement,Google,SEO,SEA,Visibilité,Référencement',
+  description:
+    'Nous développons et consolidons votre <b>visibilité</b> en appliquant les bonnes pratiques marketing si chères à <b>Google</b>. Nous étudions votre entreprise, vos projets, votre cible et votre zone de chalandise afin de mettre en avant votre site Internet en le plaçant sur les bons mots clés. Le référencement est un travail de fond et c’est parce qu’un référencement réussi est le fruit d’une préparation aboutie et réussie que nous sommes là.',
+  sites: {
+    'Agence digitale La Rochelle': 'agence-digitale-la-rochelle.fr',
+    'Agence seo La Rochelle': 'agence-seo-la-rochelle.fr',
+    'Agence web La Rochelle': 'la-rochelle-agence-web.fr'
+  }
+};
