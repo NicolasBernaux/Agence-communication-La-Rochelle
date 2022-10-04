5f19a008129fdeb54a52ba4b89a4bb3b99443897
nicolasBernaux <bernaux.n@gmail.com>
2022-10-04 03:47:22 +0200

    Fix footer links

--- content.js
+++ content.js
@@ -6,8 +6,8 @@ module.exports = {
   description:
     'Nous développons et consolidons votre <b>visibilité</b> en appliquant les bonnes pratiques marketing si chères à <b>Google</b>. Nous étudions votre entreprise, vos projets, votre cible et votre zone de chalandise afin de mettre en avant votre site Internet en le plaçant sur les bons mots clés. Le référencement est un travail de fond et c’est parce qu’un référencement réussi est le fruit d’une préparation aboutie et réussie que nous sommes là.',
   sites: {
-    'Agence digitale La Rochelle': 'agence-digitale-la-rochelle.fr',
-    'Agence seo La Rochelle': 'agence-seo-la-rochelle.fr',
-    'Agence web La Rochelle': 'la-rochelle-agence-web.fr'
+    'Agence digitale La Rochelle': 'https://agence-digitale-la-rochelle.fr',
+    'Agence seo La Rochelle': 'https://agence-seo-la-rochelle.fr',
+    'Agence web La Rochelle': 'https://la-rochelle-agence-web.fr'
   }
 };
